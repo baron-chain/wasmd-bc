1d23c7f0fe588e35ca91cf8c56ae966ce434810c
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-10-28 16:08:40 +0200

    Update test_support.go

--- app/test_support.go
+++ app/test_support.go
@@ -11,30 +11,37 @@ import (
 	"github.com/CosmWasm/wasmd/x/wasm"
 )
 
+// GetIBCKeeper returns the IBC keeper instance.
 func (app *WasmApp) GetIBCKeeper() *ibckeeper.Keeper {
 	return app.IBCKeeper
 }
 
+// GetScopedIBCKeeper returns the scoped IBC keeper instance for capability management.
 func (app *WasmApp) GetScopedIBCKeeper() capabilitykeeper.ScopedKeeper {
 	return app.ScopedIBCKeeper
 }
 
+// GetBaseApp returns the BaseApp instance.
 func (app *WasmApp) GetBaseApp() *baseapp.BaseApp {
 	return app.BaseApp
 }
 
+// GetBankKeeper returns the Bank keeper instance.
 func (app *WasmApp) GetBankKeeper() bankkeeper.Keeper {
 	return app.BankKeeper
 }
 
+// GetStakingKeeper returns the Staking keeper instance.
 func (app *WasmApp) GetStakingKeeper() *stakingkeeper.Keeper {
 	return app.StakingKeeper
 }
 
+// GetAccountKeeper returns the Account keeper instance.
 func (app *WasmApp) GetAccountKeeper() authkeeper.AccountKeeper {
 	return app.AccountKeeper
 }
 
+// GetWasmKeeper returns the Wasm keeper instance.
 func (app *WasmApp) GetWasmKeeper() wasm.Keeper {
 	return app.WasmKeeper
 }
