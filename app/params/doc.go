fe39f2478dae4cc150cdb961a931339d65e8d5d7
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-10-28 16:17:36 +0200

    Update doc.go

--- app/params/doc.go
+++ app/params/doc.go
@@ -1,19 +1,19 @@
 /*
-Package params defines the simulation parameters in the gaia.
+Package params defines the simulation parameters in Gaia.
 
-It contains the default weights used for each transaction used on the module's
-simulation. These weights define the chance for a transaction to be simulated at
-any gived operation.
+It contains the default weights used for each transaction in the module's
+simulation. These weights determine the probability of a transaction being simulated
+for any given operation.
 
-You can repace the default values for the weights by providing a params.json
-file with the weights defined for each of the transaction operations:
+You can replace the default values of the weights by providing a `params.json` file
+with the weights defined for each transaction operation:
 
 	{
 		"op_weight_msg_send": 60,
-		"op_weight_msg_delegate": 100,
+		"op_weight_msg_delegate": 100
 	}
 
-In the example above, the `MsgSend` has 60% chance to be simulated, while the
-`MsgDelegate` will always be simulated.
+In the example above, the `MsgSend` has a 60% chance of being simulated, while the
+`MsgDelegate` will always be simulated (100% chance).
 */
 package params
