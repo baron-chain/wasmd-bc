bd1fe9c2fa072c576a9ee197fbe0c0e3619cddc6
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-10-28 16:11:03 +0200

    Update genesis.go

--- app/genesis.go
+++ app/genesis.go
@@ -6,16 +6,12 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 )
 
-// GenesisState of the blockchain is represented here as a map of raw json
-// messages key'd by a identifier string.
-// The identifier is used to determine which module genesis information belongs
-// to so it may be appropriately routed during init chain.
-// Within this application default genesis information is retrieved from
-// the ModuleBasicManager which populates json from each BasicModule
-// object provided to it during init.
+// GenesisState represents the initial state of the blockchain, where each module's
+// genesis data is stored as a raw JSON message, mapped by module name.
 type GenesisState map[string]json.RawMessage
 
-// NewDefaultGenesisState generates the default state for the application.
+// NewDefaultGenesisState generates the default genesis state for the application
+// by using the ModuleBasicManager to retrieve the default genesis for each module.
 func NewDefaultGenesisState(cdc codec.JSONCodec) GenesisState {
 	return ModuleBasics.DefaultGenesis(cdc)
 }
