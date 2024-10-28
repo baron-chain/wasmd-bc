d1ea4908d8e0a7e707333b7631cc8ed374ceac77
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-10-28 16:05:39 +0200

    Update wasm.go

--- app/wasm.go
+++ app/wasm.go
@@ -1,8 +1,8 @@
 package app
 
-// AllCapabilities returns all capabilities available with the current wasmvm
-// See https://github.com/CosmWasm/cosmwasm/blob/main/docs/CAPABILITIES-BUILT-IN.md
-// This functionality is going to be moved upstream: https://github.com/CosmWasm/wasmvm/issues/425
+// AllCapabilities returns a list of all capabilities available with the current WasmVM.
+// Reference: https://github.com/CosmWasm/cosmwasm/blob/main/docs/CAPABILITIES-BUILT-IN.md
+// Note: This functionality will be moved upstream: https://github.com/CosmWasm/wasmvm/issues/425
 func AllCapabilities() []string {
 	return []string{
 		"iterator",
