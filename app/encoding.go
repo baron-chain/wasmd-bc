237178a234a80f1ccae8fbba0b89ea846fd0c7f6
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-10-28 16:12:47 +0200

    Update encoding.go

--- app/encoding.go
+++ app/encoding.go
@@ -2,16 +2,20 @@ package app
 
 import (
 	"github.com/cosmos/cosmos-sdk/std"
-
 	"github.com/CosmWasm/wasmd/app/params"
 )
 
-// MakeEncodingConfig creates a new EncodingConfig with all modules registered
+// MakeEncodingConfig creates and returns a new EncodingConfig with all modules registered.
 func MakeEncodingConfig() params.EncodingConfig {
 	encodingConfig := params.MakeEncodingConfig()
+
+	// Register legacy amino codec and interfaces
 	std.RegisterLegacyAminoCodec(encodingConfig.Amino)
 	std.RegisterInterfaces(encodingConfig.InterfaceRegistry)
+
+	// Register module-specific codecs and interfaces
 	ModuleBasics.RegisterLegacyAminoCodec(encodingConfig.Amino)
 	ModuleBasics.RegisterInterfaces(encodingConfig.InterfaceRegistry)
+
 	return encodingConfig
 }
